ae53f2b4f715a153e475ec1c6e5d60733cd3dae7
BoontonHighSchool <35753476+BoontonHighSchool@users.noreply.github.com>
2023-01-17 09:20:25 -0500

    Added player

--- Player.gd
+++ Player.gd
@@ -1,16 +1,24 @@
 extends KinematicBody2D
 
+var speed = 200
+var rotation_speed = 1.5
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocity = Vector2.ZERO
+var rotation_dir = 0
 
+func get_input():
+	rotation_dir = 0
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed('ui_right'):
+		rotation_dir += 1
+	if Input.is_action_pressed('ui_left'):
+		rotation_dir -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity -= transform.x * speed
+	if Input.is_action_pressed('ui_up'):
+		velocity += transform.x * speed
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	get_input()
+	rotation += rotation_dir * rotation_speed * delta
+	velocity = move_and_slide(velocity)
